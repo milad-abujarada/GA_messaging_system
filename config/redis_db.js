621d240b62274d7878da0607ac113674f0c2644b
miladabujarada <milad.abujarada@gmail.com>
2018-02-28 11:55:06 -0700

    added client.auth in redis_db.js

--- config/redis_db.js
+++ config/redis_db.js
@@ -3,6 +3,7 @@ const url = require('url');
 //const client = redis.createClient();
 const redisURL = url.parse(process.env.REDISCLOUD_URL);
 const client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 client.on('connect', () => console.log('connected to redis'));
 
