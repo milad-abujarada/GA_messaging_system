95b3ecb266f3c3c82ea5f8f3064df568b5c21d34
miladabujarada <milad.abujarada@gmail.com>
2018-02-28 11:39:16 -0700

    redisCloud configuration changes

--- config/redis_db.js
+++ config/redis_db.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
-const client = redis.createClient();
+//const client = redis.createClient();
+var client = redis.createClient((process.env.REDISCLOUD_URL, {no_ready_check: true}) || 3000);
 
 client.on('connect', () => console.log('connected to redis'));
 
